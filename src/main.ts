995f217e88d1db4bfa7e0679ac3e7ce94ec56c96
Miguel <migueltrrds@gmail.com>
2025-08-11 12:03:55 +0000

    correção de erro

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Toast, { type PluginOptions } from "vue-toastification";
+import Toast, { type PluginOptions, POSITION } from "vue-toastification";
 
 import "vue-toastification/dist/index.css";
 import './assets/main.css'
@@ -11,7 +11,7 @@ const options: PluginOptions = {
     transition: "Vue-Toastification__bounce",
     maxToasts: 5,
     newestOnTop: true,
-    position: "top-right",
+    position: POSITION.TOP_RIGHT,
     timeout: 4000,
     closeOnClick: true,
     pauseOnFocusLoss: true,
